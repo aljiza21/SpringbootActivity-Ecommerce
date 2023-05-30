61e77f161d43bb2a5a33b029c71fe353ea0ce360
Charlotte-code9 <medallacharlotte7@gmail.com>
2023-05-30 17:51:47 +0800

    Modify some changes in login and repository

--- src/main/java/com/springbootecommerce/service/ProductServiceImplementation.java
+++ src/main/java/com/springbootecommerce/service/ProductServiceImplementation.java
@@ -2,6 +2,7 @@ package com.springbootecommerce.service;
 
 import com.springbootecommerce.entity.Product;
 import com.springbootecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.Optional;
 @Service
 public class ProductServiceImplementation implements ProductService{
 
+    @Autowired
     private ProductRepository productRepository;
 
     @Override
